56cd8e96366d6c8dcd7b8acc728500ba3e8719b3
saybeom <tpgml2379@gmail.com>
2024-03-06 23:00:22 +0900

    analysis and main add

---
+++ js/common.js
@@ -0,0 +1,22 @@
+const basic = {
+    "title": '박석범 & 장세희 결혼합니다.',
+    "groom": '박석범',
+    "bride": '장세희',
+    "date": {
+        "year" : '2024',
+        "month" : '09',
+        "date" : '28',
+        "hour" : '12',
+        "minute" : '40',
+    },
+    "location": {
+        "name" : "루이비스 대전",
+        "address" : "",
+        "x" : "",
+        "y" : ""
+    }
+}
+
+$(document).ready(function (){
+
+});
