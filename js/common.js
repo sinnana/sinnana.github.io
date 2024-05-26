3f3076cf7c93c7615fa37244133334f1b6d44c9d
saybeom <tpgml2379@gmail.com>
2024-05-26 22:45:13 +0900

    scroll-y modify

--- js/common.js
+++ js/common.js
@@ -195,14 +195,14 @@ function setGallery(id){
     let parentElement = document.getElementById(id);
 
     let randomImageNumbers = [];
-    randomImageNumbers = Array.from({length: numOfImages}, (_, index) => index + 1);
-    // 랜덤 이미지 번호 배열 생성
-    // while (randomImageNumbers.length < numOfImages) {
-    //     let randomNum = Math.floor(Math.random() * numOfImages) + 1;
-    //     if (!randomImageNumbers.includes(randomNum)) {
-    //         randomImageNumbers.push(randomNum);
-    //     }
-    // }
+    //randomImageNumbers = Array.from({length: numOfImages}, (_, index) => index + 1);
+    //랜덤 이미지 번호 배열 생성
+    while (randomImageNumbers.length < numOfImages) {
+        let randomNum = Math.floor(Math.random() * numOfImages) + 1;
+        if (!randomImageNumbers.includes(randomNum)) {
+            randomImageNumbers.push(randomNum);
+        }
+    }
 
     let cnt = 0;
     randomImageNumbers.forEach(function(imageNumber) {
