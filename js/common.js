c807b05e6734ad8487dd0e90629406205d757daf
saybeom <tpgml2379@gmail.com>
2024-05-26 23:20:07 +0900

    magnificPopup callbacks modify

--- js/common.js
+++ js/common.js
@@ -101,7 +101,17 @@ $(document).ready(function (){
         gallery: {
             enabled: true
         },
-        type: 'image'
+        type: 'image',
+        callbacks: {
+            beforeOpen: function () {
+                $('body').css("overflow", "hidden");
+                $('body').css("touch-action", "none");
+            },
+            close: function() {
+                $('body').css("overflow", "");
+                $('body').css("touch-action", "pan-y");
+            },
+        }
     });
 
     drawCalendar(window.basic.date);
@@ -214,14 +224,14 @@ function setGallery(id){
     let parentElement = document.getElementById(id);
 
     let randomImageNumbers = [];
-    randomImageNumbers = Array.from({length: numOfImages}, (_, index) => index + 1);
+    //randomImageNumbers = Array.from({length: numOfImages}, (_, index) => index + 1);
     //랜덤 이미지 번호 배열 생성
-    // while (randomImageNumbers.length < numOfImages) {
-    //     let randomNum = Math.floor(Math.random() * numOfImages) + 1;
-    //     if (!randomImageNumbers.includes(randomNum)) {
-    //         randomImageNumbers.push(randomNum);
-    //     }
-    // }
+    while (randomImageNumbers.length < numOfImages) {
+        let randomNum = Math.floor(Math.random() * numOfImages) + 1;
+        if (!randomImageNumbers.includes(randomNum)) {
+            randomImageNumbers.push(randomNum);
+        }
+    }
 
     let cnt = 0;
     randomImageNumbers.forEach(function(imageNumber) {
